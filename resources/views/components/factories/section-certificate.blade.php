ded7da84065dfdd082d87469d8086faaca2a0dd4
Yohanes Chandra Wijaya <yohaneschandrawijaya@Yohaness-MacBook-Air.local>
2024-09-15 23:55:53 +0700

    feat:[YC] added factories page

---
+++ resources/views/components/factories/section-certificate.blade.php
@@ -0,0 +1,15 @@
+<section class="certificate">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-7 col-lg-8 mb-4 mb-md-0 d-flex align-items-center">
+                <div class="content">
+                    <h2 class="mb-4">Certifications</h2>
+                    <p>Our furniture manufacturing company proudly holds a legal wood certification, which guarantees that we source our raw materials responsibly and ethically. This certification not only signifies that we use high-quality wood but also ensures that all our materials are sourced legally and sustainably.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-5 col-lg-4">
+                <img src="{{ asset('img/logo-svlk.png') }}" class="w-100 d-block mx-auto img-fluid" alt="SVLK" />
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/components/factories/section-features.blade.php
@@ -0,0 +1,27 @@
+<section class="features">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6 offset-sm-3 col-md-4 offset-md-0">
+                <div class="tile--features">
+                    <img src="{{ asset('img/logo-black.png') }}" class="w-75 d-block mx-auto img-fluid" alt="Features" />
+                    <h3>5,000 sqm</h3>
+                    <p>Factory spaces equipped with top-notch standards</p>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 offset-sm-3 col-md-4 offset-md-0">
+                <div class="tile--features">
+                    <img src="{{ asset('img/logo-black.png') }}" class="w-75 d-block mx-auto img-fluid" alt="Features" />
+                    <h3>30+ Containers</h3>
+                    <p>Export containers annualy for hospitality project, custom made exported to project owners around the world</p>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 offset-sm-3 col-md-4 offset-md-0">
+                <div class="tile--features">
+                    <img src="{{ asset('img/logo-black.png') }}" class="w-75 d-block mx-auto img-fluid" alt="Features" />
+                    <h3>100+ experts</h3>
+                    <p>We are continually enhancing our craftsmanship and dedicating our teams to deliver world-class quality.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
