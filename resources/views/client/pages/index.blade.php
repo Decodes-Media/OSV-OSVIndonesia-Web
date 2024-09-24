48e616004ec91e9d0649207959246f29716d35e8
Yohanes Chandra Wijaya <yohaneschandrawijaya@Yohaness-MacBook-Air.local>
2024-09-24 15:44:14 +0700

    fix:[YC] update feedback mom 20 aug

--- resources/views/client/pages/index.blade.php
+++ resources/views/client/pages/index.blade.php
@@ -3,6 +3,7 @@
     <x-home.section-highlights />
     <!-- <x-home.section-video /> -->
     <x-home.section-services />
+    <x-home.section-running-text />
     <x-home.section-intro />
     <x-home.section-product-showcase />
     <x-global.section-clients />
