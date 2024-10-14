4eac7d25ed2689b01d5af886da4e03e8043d5b87
Yohanes Chandra Wijaya <yohaneschandrawijaya@Yohaness-MacBook-Air.local>
2024-10-14 12:22:11 +0700

    feat: [YC] add new clients logo

--- resources/views/components/global/section-clients.blade.php
+++ resources/views/components/global/section-clients.blade.php
@@ -8,52 +8,67 @@
         <div class="row justify-content-center" data-aos="fade-in">
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-1.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/OBLU-NATURE-Helengeli.jpg') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-2.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/305.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-3.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Angsana.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-4.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Baros.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-5.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Embudu.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-6.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Huvafen_Fushi.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-7.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Niyama-logo.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-8.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Radisson_Blu_logo.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-9.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/Rove-Al-Marjan-Island-Hotel.png') }}" alt="Client Logo" />
                 </div>
             </div>
             <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
                 <div class="tile--client-logo">
-                    <img src="{{ asset('img/client-10.png') }}" alt="Client Logo" />
+                    <img src="{{ asset('img/clients/sofitel-fiji-logo.png') }}" alt="Client Logo" />
+                </div>
+            </div>
+            <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
+                <div class="tile--client-logo">
+                    <img src="{{ asset('img/clients/ULA-logo.png') }}" alt="Client Logo" />
+                </div>
+            </div>
+            <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
+                <div class="tile--client-logo">
+                    <img src="{{ asset('img/clients/Velaa.png') }}" alt="Client Logo" />
+                </div>
+            </div>
+            <div class="col-4 col-md-3 col-lg-2 d-flex align-items-center">
+                <div class="tile--client-logo">
+                    <img src="{{ asset('img/clients/Waldorf-Astoria-Seychelles.png') }}" alt="Client Logo" />
                 </div>
             </div>
         </div>
