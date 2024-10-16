f4f438d51090dbe4183e6679c292d94db8e76d55
yohanes <yohanes.chandra@talenavi.com>
2024-10-16 12:40:30 +0700

    fix: [YC] update mom 16 oct

--- resources/views/components/contact/section-contact-info.blade.php
+++ resources/views/components/contact/section-contact-info.blade.php
@@ -6,20 +6,16 @@
                 <form method="POST" onsubmit="" class="form mb-5">
                     <div class="row">
                         <div class="col-12 col-md-6">
-                            <label>Full Name<span class="text-danger">*</span></label>
-                            <input id="fullName" type="text" class="form-control" name="fullName" placeholder="eg. John Doe" required>
+                            <input id="fullName" type="text" class="form-control" name="fullName" placeholder="Full Name" required>
                         </div>
                         <div class="col-12 col-md-6">
-                            <label>Phone Number<span class="text-danger">*</span></label>
-                            <input id="phone" type="number" class="form-control" name="phone" placeholder="eg. 081234567890" required>
+                            <input id="phone" type="number" class="form-control" name="phone" placeholder="Phone Number" required>
                         </div>
                         <div class="col-12">
-                            <label>Country<span class="text-danger">*</span></label>
-                            <input id="country" type="text" class="form-control" name="country" placeholder="eg. Indonesia" required>
+                            <input id="country" type="text" class="form-control" name="country" placeholder="Country" required>
                         </div>
                         <div class="col-12">
-                            <label>Company Name<span class="text-danger">*</span></label>
-                            <input id="companyName" type="text" class="form-control" name="companyName" placeholder="eg. ABC Company" required>
+                            <input id="companyName" type="text" class="form-control" name="companyName" placeholder="Company Name" required>
                         </div>
                         <div class="col-12">
                             <textarea id="message" class="form-control" name="message" rows="5" placeholder="Write your message.." required></textarea>
